097e4c6348d648d068b902f4e99d682bdcacf27a
l-marcel <lmgh1312@gmail.com>
2025-05-01 11:31:43 -0300

    [test] Finalizando testes do padrão adaptador

--- adapter/src/test/java/app/clients/ClientTest.java
+++ adapter/src/test/java/app/clients/ClientTest.java
@@ -1,18 +1,24 @@
 package app.clients;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import app.BaseTest;
 import app.adapters.SchedulerToExecutor;
 import app.adapters.SchedulerToTimer;
+import app.errors.InvalidEventTime;
 import app.events.NotificationEvent;
 
 public class ClientTest extends BaseTest {
     @Test
     public void mustBeAbleToShedule() {
-        //[TODO] Terminar os testes do cliente
-        
         Client a = new Client(new SchedulerToTimer());
         Client b = new Client(new SchedulerToTimer());
         Client c = new Client(new SchedulerToExecutor());
@@ -27,5 +33,34 @@ public class ClientTest extends BaseTest {
             "Teste 2", 
             "Um teste qualquer!"
         ));
+
+        Instant now = Instant.now();
+        assertThrows(
+            InvalidEventTime.class,
+            () -> {
+                a.schedule(firstEvent, now.plusSeconds(-1));
+            }
+        );
+        
+        a.schedule(firstEvent, now.plusSeconds(2));
+        b.schedule(firstEvent, now.plusSeconds(2));
+        c.schedule(firstEvent, now.plusSeconds(3));
+        d.schedule(firstEvent, now.plusSeconds(3));
+        a.schedule(secondEvent, now.plusSeconds(3));
+        b.schedule(secondEvent, now.plusSeconds(3));
+        c.schedule(secondEvent, now.plusSeconds(4));
+        d.schedule(secondEvent, now.plusSeconds(4));
+
+        Duration duration = Duration.between(now, now.plusSeconds(2));
+        Awaitility
+            .await()
+            .atLeast(duration.minusSeconds(1))
+            .atMost(duration.plusSeconds(5))
+            .untilAsserted(
+                () -> {
+                    Mockito.verify(firstEvent, times(4)).run();
+                    Mockito.verify(secondEvent, times(4)).run();
+                }
+            );
     };
 };
